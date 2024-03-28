1f2a06a26ec844f0487995064744cbe2fbd26c2e
ch1nya <you@example.com>
2024-03-28 19:45:59 +0300

    added mailing feature

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, {useRef, useState} from "react";
 import { FaCheck } from "react-icons/fa";
+import * as emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
+      user_name: "",
+      user_email: "",
+      message: "",
   });
   const [submitted, setSubmitted] = useState(false);
 
@@ -17,62 +18,72 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted!");
-    setSubmitted(true);
+
+  const sendEmail = (e) => {
+      e.preventDefault();
+      emailjs
+          .sendForm(
+              {serviceId},
+              {templateId},
+              e.target, {publicKey: {publicKey}})
+          .then(
+              (result) => {
+                  console.log(result.text);
+                  console.log('SUCCESS!');},
+              (error) => {
+                  console.log('FAILED...', error.text);},
+          );
+      setSubmitted(true);
   };
 
+
   return (
-    <div className="contact-container">
-      <h2>Contact Me</h2>
-      {submitted ? (
-        <p className="submitted-message">Form has been submitted!</p>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <a type="submit" href="mailTo:chiniakinivan@gmail.ru">
-            <button className="submit-btn">
-              {" "}
-              Submit
-              <FaCheck> </FaCheck>{" "}
-            </button>
-            {/*//TODO проверить работоспособность*/}
-          </a>
-        </form>
-      )}
-    </div>
+      <div className="contact-container">
+        <h2>Contact Me</h2>
+        {submitted ? (
+            <p className="submitted-message" >Form has been submitted!</p>
+        ) : (
+            <form onSubmit={sendEmail}>
+              <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    name="user_name"
+                    id="name"
+                    value={formData.user_name}
+                    onChange={handleChange}
+                    required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    name="user_email"
+                    id="email"
+                    value={formData.user_email}
+                    onChange={handleChange}
+                    required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message:</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                />
+              </div>
+              <button type="submit" value="Send" className="submit-btn">
+                {" "}
+                Submit
+                <FaCheck> </FaCheck>{" "}
+              </button>
+            </form>
+        )}
+      </div>
   );
 };
 
